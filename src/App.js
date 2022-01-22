f1136d90ffa31a102c0a86cab414bbb06e9f5895
Janno Oolo <janno.oolo@nixor.eu>
2022-01-22 19:00:01 +0200

    added card componenst

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import logo from "./logo.svg";
 import { Component } from "react/cjs/react.production.min";
 import "./App.css";
+import { CardList } from "./components/card-list/card-list.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: "HEllo Janno",
+      monsters: [],
     };
   }
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: "hello Oolo" })}>
-            Change text
-          </button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CardList monsters={this.state.monsters} />
       </div>
     );
   }
