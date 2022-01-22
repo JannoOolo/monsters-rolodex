ecea30cad3f7ab25b28ee0f88642aaebbd51091a
Janno Oolo <janno.oolo@nixor.eu>
2022-01-22 16:51:27 +0200

    added prettier and format on save. Reformated code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import logo from './logo.svg';
-import { Component } from 'react/cjs/react.production.min';
-import './App.css';
+import logo from "./logo.svg";
+import { Component } from "react/cjs/react.production.min";
+import "./App.css";
 
-class App extends Component{
-  constructor(){
-    super( );
-    this.state ={
-      string: 'HEllo Janno'
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      string: "HEllo Janno",
     };
   }
-  render(){
+  render() {
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{ this.state.string }</p>
-        <button onClick={()=>this.setState({string:'hello Oolo'})}>Change text</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  )}
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.string}</p>
+          <button onClick={() => this.setState({ string: "hello Oolo" })}>
+            Change text
+          </button>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
